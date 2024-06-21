c9ab8e93d19e923d17b05acfa13335d806a3b0a4
Tobias <96661824+CrazyWolf13@users.noreply.github.com>
2024-06-21 11:45:36 +0200

    Refactor on arm, shortcut for wt

--- better-sandbox/boot.ps1
+++ better-sandbox/boot.ps1
@@ -20,18 +20,6 @@ $backgroundCommands = @(
 $backgroundScriptBlock = [scriptblock]::Create(($backgroundCommands -join "; "))
 
 
-# Custom Commands to be executed after finishing the installation
-$commands = @{
-    "AMD64" = @(
-        "wt.exe -p 'Windows Terminal'"
-    )
-    "ARM64" = @(
-        "wt.exe -p 'Windows Terminal'"
-    )
-}
-$scriptBlock = [scriptblock]::Create(($commands[$architecture] -join "; "))
-
-
 # List of URLs with a secondary property for filename
 $urls = @{
     "AMD64" = @(
@@ -93,7 +81,9 @@ foreach ($url in $urls[$architecture]) {
 Write-Host "Waiting for completion..."
 [System.Threading.Tasks.Task]::WaitAll($downloadTasks)
 
-# Custom installer commands based on architecture
+
+# Post-Installation commands
+# Execute the custom commands per architecture
 if ($architecture -eq "ARM64") {
     Write-Host "Executing ARM64 specific commands"
     Expand-Archive -Path "microsoft.ui.xaml.2.8.6.zip" -DestinationPath "microsoft.ui.xaml.2.8.6"
@@ -106,21 +96,36 @@ if ($architecture -eq "ARM64") {
     $env:Path += ";$wt_destinationPath"
     [System.Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
     Write-Host "Windows Terminal installed to $wt_destinationPath"
+    Start-Process powershell -ArgumentList "-Command wt.exe -p 'Windows Terminal'"
+
+    # Path to wt.exe
+    $wtExecutablePath = Join-Path -Path $wt_destinationPath -ChildPath "wt.exe"
+    # Pfad zum Startmenü-
+    $startMenuPath = [System.Environment]::GetFolderPath("CommonStartMenu")
+    # Vollständiger Pfad, wo die Verknüpfung erstellt werden soll
+    $linkPath = Join-Path -Path $startMenuPath -ChildPath "Programs\Windows Terminal.lnk"
+    # Erstellen der Verknüpfung
+    $shell = New-Object -ComObject WScript.Shell
+    $shortcut = $shell.CreateShortcut($linkPath)
+    $shortcut.TargetPath = $wtExecutablePath
+    $shortcut.Save()
+    Write-Host "Created a shortcut to Windows Terminal in the Start Menu."
+
 } elseif ($architecture -eq "AMD64") {
     Write-Host "Executing AMD64 specific commands"
+    Start-Process powershell -ArgumentList "-Command wt.exe -p 'Windows Terminal'"
 }
 
 Write-Host "Install the Packages"
 foreach ($file in $packagePaths[$architecture]) {
     Add-AppxPackage $file
+    Write-Host "Installed $file"
+    Write-Host "\n"
 }
 
 Write-Host "Downloads and installation completed successfully."
 
-# Post-Installation commands
-# Execute the custom commands per architecture
-Start-Process powershell -ArgumentList "-Command $scriptBlock" -Wait
-
 # Run the following commands in the background and exit the main script
+Write-Host "Executing the background commands"
 Start-Process -FilePath "powershell.exe" -ArgumentList "-Command $backgroundScriptBlock" -WindowStyle Hidden
 [Environment]::Exit(1)
